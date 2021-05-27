59c2b5e9bf3fbcbc52409b019d991b2a5432dc44
Kilian Vounckx <r0754877@esat.kuleuven.be>
2021-05-27 18:09:03 +0200

    Add main loop

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+pub fn main() !void {
+    var buffer: [100]u8 = undefined;
+    while (true) {
+        try stdout.print("calc> ", .{});
+        const input = (try stdin.readUntilDelimiterOrEof(&buffer, '\n')).?;
+        try stdout.print("{}\n", .{input});
+    }
 }
