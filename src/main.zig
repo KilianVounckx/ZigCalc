7016b19c051369ee9bfbe4a3a543ba1f35eccb46
Kilian Vounckx <r0754877@esat.kuleuven.be>
2021-06-07 21:54:26 +0200

    Add main loop

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,17 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
+const stderr = std.io.getStdErr().writer();
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+pub fn main() !void {
+    var buffer: [128]u8 = undefined;
+    while (true) {
+        try stdout.print("calc> ", .{});
+        const line = (stdin.readUntilDelimiterOrEof(&buffer, '\n') catch |err| {
+            try stderr.print("Input too long.\n", .{});
+            continue;
+        }).?;
+
+        try stdout.print("{s}\n", .{line});
+    }
 }
